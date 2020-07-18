3fcaf85e4843749bc6264546a18e28cebce8c28b
shyamajay <nehrushyam12@gmail.com>
2020-07-19 01:12:08 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<math.h>
 
 int fib(int n)
 {
     long long int a=0,b=1;
     long long int c=0;
-    while(n>=0)
+    while(n>1)
     {
         c=a+b;
         a=b;
@@ -16,7 +17,6 @@ int fib(int n)
     return c;
 }
 
-
 int fib_fast(int n)
 {
     int *a;
@@ -24,20 +24,21 @@ int fib_fast(int n)
     a = (int*)malloc(sizeof(int)*(n+2));
     a[0]=0;
     a[1]=1;
-    for(int i=2;i<=n+2;i++)
+    for(int i=2;i<=n;i++)
     {
         a[i]=a[i-1]+a[i-2];
     }
-    d = (long long)a[n+2];
+    d = (long long)a[n];
     return d;
 }
+
 
 int main()
 {
     while(true)
     {
         long int n;
-        n = rand()%1000 + 2;
+        n = rand()%10 + 2;
         printf("%ld\n",n);
         long long int res = fib(n);
         long long int res1 = fib_fast(n);
